de555983cb366bda71a13cf8590968d29d621758
Owner <97609133+go-compile@users.noreply.github.com>
2022-01-13 18:30:27 +0000

    ADD: decrypt cli option

---
+++ cmd/qrsecrets/decrypt.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/1william1/ecc"
+	"github.com/chzyer/readline"
+	"github.com/go-compile/qrsecrets"
+	"github.com/liyue201/goqr"
+
+	_ "image/jpeg"
+	_ "image/png"
+)
+
+func decrypt(options *options, prompt *readline.Instance, privateKeyFile string, inputFile string) error {
+
+	priv, err := readPrivateKey(privateKeyFile, prompt)
+	if err != nil {
+		return err
+	}
+
+	// Convert ECDSA private key to ECC private key
+	key := &ecc.Private{
+		D: priv.D,
+		Public: &ecc.Public{
+			Curve: priv.Curve,
+			X:     priv.X,
+			Y:     priv.Y,
+		},
+	}
+
+	fmt.Printf("[Info] Opening %s\n", inputFile)
+
+	f, err := os.OpenFile(inputFile, os.O_RDONLY, os.ModeExclusive)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	content := []byte{}
+	switch strings.ToLower(filepath.Ext(inputFile)) {
+	case ".png", ".jpg", "jpeg":
+		content, err = decodeQRcode(f)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("[Info] Scanned QR code and obtained %d bytes\n", len(content))
+	default:
+		content, err = ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("[Info] Opened file and read %d bytes\n", len(content))
+	}
+
+	// If master key has been set via the CLI don't ask for it again
+	masterKey := []byte(options.masterkey)
+	if options.masterkey == "" {
+		fmt.Println("Input your master key:")
+		masterKey, err = prompt.ReadPassword("Master key: ")
+		if err != nil {
+			return err
+		}
+	}
+
+	container, err := qrsecrets.UnmarshalContainer(content, key, masterKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[Info] Success, decrypted %d bytes:\n", len(container.CipherText.Plaintext))
+	fmt.Println(string(container.CipherText.Plaintext))
+
+	return nil
+}
+
+func decodeQRcode(f io.Reader) ([]byte, error) {
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+
+	codes, err := goqr.Recognize(img)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(codes) < 1 {
+		return nil, errors.New("no qr code found in image")
+	}
+
+	return codes[0].Payload, nil
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/1william1/ecc v1.0.1
 	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e
+	github.com/liyue201/goqr v0.0.0-20200803022322-df443203d4ea
 	github.com/pkg/errors v0.9.1
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
